706caac1ded637d91ce45db4af46ab0fa1a152db
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-08-10 10:45:07 +0530

    runtime: windows DLL call - minor refactor
    Refactor len(a) to the top of the method. This will be optimized away since it'll likely just be read from the CPU cache, but it's always better to remove instructions from the hot path even if it's just one. And the code also looks nicer.

--- src/syscall/dll_windows.go
+++ src/syscall/dll_windows.go
@@ -139,41 +139,42 @@ func (p *Proc) Addr() uintptr {
 // (according to the semantics of the specific function being called) before consulting
 // the error. The error will be guaranteed to contain syscall.Errno.
 func (p *Proc) Call(a ...uintptr) (r1, r2 uintptr, lastErr error) {
-	switch len(a) {
+	l := len(a)
+	switch l {
 	case 0:
-		return Syscall(p.Addr(), uintptr(len(a)), 0, 0, 0)
+		return Syscall(p.Addr(), uintptr(l), 0, 0, 0)
 	case 1:
-		return Syscall(p.Addr(), uintptr(len(a)), a[0], 0, 0)
+		return Syscall(p.Addr(), uintptr(l), a[0], 0, 0)
 	case 2:
-		return Syscall(p.Addr(), uintptr(len(a)), a[0], a[1], 0)
+		return Syscall(p.Addr(), uintptr(l), a[0], a[1], 0)
 	case 3:
-		return Syscall(p.Addr(), uintptr(len(a)), a[0], a[1], a[2])
+		return Syscall(p.Addr(), uintptr(l), a[0], a[1], a[2])
 	case 4:
-		return Syscall6(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], 0, 0)
+		return Syscall6(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], 0, 0)
 	case 5:
-		return Syscall6(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], 0)
+		return Syscall6(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], 0)
 	case 6:
-		return Syscall6(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5])
+		return Syscall6(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5])
 	case 7:
-		return Syscall9(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], 0, 0)
+		return Syscall9(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], 0, 0)
 	case 8:
-		return Syscall9(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], 0)
+		return Syscall9(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], 0)
 	case 9:
-		return Syscall9(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8])
+		return Syscall9(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8])
 	case 10:
-		return Syscall12(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], 0, 0)
+		return Syscall12(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], 0, 0)
 	case 11:
-		return Syscall12(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], 0)
+		return Syscall12(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], 0)
 	case 12:
-		return Syscall12(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11])
+		return Syscall12(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11])
 	case 13:
-		return Syscall15(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], 0, 0)
+		return Syscall15(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], 0, 0)
 	case 14:
-		return Syscall15(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], 0)
+		return Syscall15(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], 0)
 	case 15:
-		return Syscall15(p.Addr(), uintptr(len(a)), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14])
+		return Syscall15(p.Addr(), uintptr(l), a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14])
 	default:
-		panic("Call " + p.Name + " with too many arguments " + itoa(len(a)) + ".")
+		panic("Call " + p.Name + " with too many arguments " + itoa(l) + ".")
 	}
 }
 
